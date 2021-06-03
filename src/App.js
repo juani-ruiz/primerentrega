1742148daf500aa4837f0c6383b09704a292360c
juani-ruiz <juaniito005@gmail.com>
2021-06-03 14:22:54 -0300

    modificacion navbar

--- src/App.js
+++ src/App.js
@@ -4,21 +4,39 @@ import './css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ul class="nav justify-content-left">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">  
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     
+      </button>    
+      
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link active" href="">Home</a>
           </li>
+          
           <li class="nav-item">
-            <a class="nav-link" href="#">informacion</a> 
+            <a class="nav-link active" aria-current="page" href="">Productos</a>
           </li>
+          
           <li class="nav-item">
-            <a class="nav-link" href="#">contacto</a>
-          </li>          
-      </ul>      
-      </header>
+            <a class="nav-link active" aria-current="page" href="">Preguntas Frecuentes</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link active" href="">Contacto</a>
+          </li>
+        
+        </ul>  
+       
+      </div>
     </div>
+  </nav>
+
+
+
   );
 }
 
